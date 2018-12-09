1759db19f8de80f8b5a4c63569e4a10a541b44b4
Jon Lim <jonconneryl@gmail.com>
2018-12-09 02:10:30 +0100

    TODO Base assignment done

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 var addButton = document.getElementById("add");
 var taskInput = document.getElementById("input");
 var taskList = document.getElementById("taskList");
+var clearButton = document.getElementById("clear");
+
+addButton.addEventListener("click", function(){
+    var task = taskInput.value;
+    if(task.trim()){
+        var newItem = document.createElement("LI");
+        var taskText = document.createTextNode(task);
+        newItem.appendChild(taskText);
+        taskInput.value = "";
+        var removeButton = document.createElement("BUTTON");
+        removeButton.innerHTML = "DONE";
+        removeButton.className = "remove";
+        removeButton.addEventListener("click", removeTask);
+        newItem.appendChild(removeButton);
+        taskList.appendChild(newItem);
+    }
+    else{
+        alert("Task cannot be empty!");
+    }
+}
+);
+
+clearButton.addEventListener("click", function(){
+    taskList.innerHTML = "";
+}
+);
+
+function removeTask(e){
+    var taskItem = e.target.parentElement;
+    taskList.removeChild(taskItem);
+}
